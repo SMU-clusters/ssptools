fix bugs surrounding out-of-bounds mto computation
Now that the usual check for time vs `tms_u` before adjusting the
mto bounds are removed, the actual copmutation of the mto and the
determination/adjustment of the corresponding mass bounds, needs
to be adjusted to correctly handle cases outside the norm.

The `compute_mto` equation is invalid below the maximum at `a[0]`,
and in the past relied on the fact that the lowest mass bound
was typically above that, but it was never guaranteed. Now it will
explicitly go to inf, and the bins will not be adjusted when
outside the bounds

ssptools/evolve_mf.py
@@ -295,8 +295,14 @@ class EvolvedMF:
 
     def compute_mto(self, t):
         '''Compute the turn-off mass for a given time `t` (inverse of tms)'''
+
         a = self._tms_constants
-        return (np.log(t / a[0]) / a[1]) ** (1 / a[2])
+
+        if t > a[0]:
+            return (np.log(t / a[0]) / a[1]) ** (1 / a[2])
+        else:
+            # Set NaN resulting from t<max (i.e. a0) to inf
+            return np.inf
 
     def _derivs(self, t, y):
         '''Main function for computing derivatives relevant to mass evolution

ssptools/masses.py
@@ -355,11 +355,13 @@ class MassBins:
         else:
             return Ns, alpha, Nwd, Nns, Nbh, Mwd, Mns, Mbh
 
-    def determine_index(self, mass, massbins):
+    def determine_index(self, mass, massbins, *, allow_overflow=False):
         '''determine the mbin index in the a given mass falls into in
         a `mbin` type set of mass bins
 
         mass bins are left-inclusive, i.e. [lower, upper)
+
+        currently only support one mass at a time, sorry
         '''
 
         # If a string labelling the class, get from stored bins
@@ -371,8 +373,14 @@ class MassBins:
         try:
             ind = np.flatnonzero(massbins.lower <= mass)[-1]
         except IndexError:
-            mssg = f"mass {mass} is outside of bounds of any bin in {massbins}"
+            mssg = f"mass {mass} is below lowest bound in {massbins}"
             raise ValueError(mssg)
+
+        # If this is the last bin, maybe check if its actually overflowing
+        if ind >= massbins.upper.size - 1:
+            if (massbins.upper[-1] <= mass) and allow_overflow is False:
+                mssg = f"mass {mass} is above highest bound in {massbins}"
+                raise ValueError(mssg)
 
         return ind
 
@@ -385,12 +393,16 @@ class MassBins:
         approximate it for use in some functions, where the mean mass of the
         bin would actually be lowered.
         '''
-        isev = self.determine_index(mto, 'MS')
 
         low, up = self.bins.MS.lower.copy(), self.bins.MS.upper.copy()
 
-        # skip if in last bin
-        if isev < self.nbin.MS - 1:
+        # Find the bin containing mto and adjust its upper bound only
+        try:
+            isev = self.determine_index(mto, 'MS')
             up[isev] = mto
+
+        # If the mto is above or below the min/max bounds, just ignore it
+        except ValueError:
+            pass
 
         return mbin(low, up)