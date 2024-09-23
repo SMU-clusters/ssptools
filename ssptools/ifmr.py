5d46c90e54dbc93165e0655eaf0697f58a910beb
Nolan Dickson <nolan.dickson@smu.ca>
2024-09-23 17:24:33 -0300

    separate WD/BH IFMR functions into own functions
    In similar fashion to the natal kicks, sets the stage for using
    other functional forms of the IFMR by separating some logic into
    smaller functions

--- ssptools/ifmr.py
+++ ssptools/ifmr.py
@@ -22,6 +22,115 @@ def get_data(path):
     return _ROOT / "data" / path
 
 
+# --------------------------------------------------------------------------
+# White Dwarf Initial-Final mass predictors
+# --------------------------------------------------------------------------
+
+
+def _MIST2018_WD_predictor(FeH):
+    '''Return func(mi), WD_mi, WD_mf based on MIST 2018 interpolations
+    '''
+
+    wdgrid = np.loadtxt(get_data("sevtables/wdifmr.dat"))
+
+    # ----------------------------------------------------------------------
+    # Check [Fe/H] is within model grid and adjust otherwise
+    # ----------------------------------------------------------------------
+
+    mssg = ("{0:.2f} is out of bounds for the {1} metallicity grid, "
+            "falling back to {2} of {3:.2f}")
+
+    if FeH < (fback := np.min(wdgrid[:, 0])):
+        logging.debug(mssg.format(FeH, 'WD', 'minimum', fback))
+        FeH = fback
+
+    elif FeH > (fback := np.max(wdgrid[:, 0])):
+        logging.debug(mssg.format(FeH, 'WD', 'maximum', fback))
+        FeH = fback
+
+    # ----------------------------------------------------------------------
+    # Compute the Polynomial fit based on the coeffs
+    # ----------------------------------------------------------------------
+
+    # Get the closest model
+    j = np.argmin(np.abs(FeH - wdgrid[:, 0]))
+    WD_m_max, WD_coeffs = wdgrid[j, 1], wdgrid[j, 2:]
+
+    WD_spline = np.polynomial.Polynomial(WD_coeffs[::-1])
+
+    # ----------------------------------------------------------------------
+    # Compute the WD initial and final mass boundaries based on the polynomial
+    # ----------------------------------------------------------------------
+
+    # TODO polynomial starts misbehaving far above 0, but don't know where
+    WD_mi = bounds(0.0, WD_m_max)
+
+    # Compute the max/mins taking by derivative of polynomial
+    WD_minmax = WD_spline.deriv().roots().real
+
+    # Restrict x (initial mass) to between 1 and max_mi
+    # TODO lower 1 is required to avoid polynomial effects, but is arbitrary
+    #   really it won't matter, maximum will always be far above 1
+    restr = (1. < WD_minmax) & (WD_minmax <= WD_m_max)
+
+    # Determine the maximum WD final mass (including upper bound in case)
+    WD_max = WD_spline(np.r_[WD_minmax[restr], WD_m_max]).max()
+
+    WD_mf = bounds(0.0, WD_max)
+
+    return WD_spline, WD_mi, WD_mf
+
+
+# --------------------------------------------------------------------------
+# Black Hole Initial-Final mass predictors
+# --------------------------------------------------------------------------
+
+
+def _F12_BH_predictor(FeH):
+
+    # ----------------------------------------------------------------------
+    # Check [Fe/H] is within model grid and adjust otherwise
+    # ----------------------------------------------------------------------
+
+    bhgrid = np.array([float(fn.stem.split('FEH')[-1])
+                       for fn in get_data('sse').glob('MP*dat')])
+
+    mssg = ("{0:.2f} is out of bounds for the {1} metallicity grid, "
+            "falling back to {2} of {3:.2f}")
+
+    if FeH < (fback := np.min(bhgrid)):
+        logging.debug(mssg.format(FeH, 'BH', 'minimum', fback))
+        FeH = fback
+
+    elif FeH > (fback := np.max(bhgrid)):
+        logging.debug(mssg.format(FeH, 'BH', 'maximum', fback))
+        FeH = fback
+
+    # ----------------------------------------------------------------------
+    # Load BH IFMR values
+    # ----------------------------------------------------------------------
+
+    # TODO if 5e-3 < FeH < 0.0, this will put wrong sign on filename
+    bhifmr = np.loadtxt(get_data(f"sse/MP_FEH{FeH:+.2f}.dat"))
+
+    # Grab only stellar type 14 (BHs)
+    BH_mi, BH_mf = bhifmr[:, :2][bhifmr[:, 2] == 14].T
+
+    # linear spline to avoid boundary effects near m_A, m_B, etc
+    BH_spline = UnivariateSpline(BH_mi, BH_mf, s=0, k=1, ext=0)
+
+    BH_mi = bounds(BH_mi[0], BH_mi[-1])
+
+    BH_mf = bounds(np.min(BH_mf), np.inf)
+
+    return BH_spline, BH_mi, BH_mf
+
+
+# --------------------------------------------------------------------------
+# Combined IFMR class for all remnant types
+# --------------------------------------------------------------------------
+
+
 class IFMR:
     '''
     Provides a class for the initial-final mass of all stellar remnants.
@@ -52,37 +161,17 @@ class IFMR:
         # Black Holes
         # ------------------------------------------------------------------
 
-        # TODO if 5e-3 < FeH < 0.0, this will put wrong sign on filename
-        bhifmr = np.loadtxt(get_data(f"sse/MP_FEH{self.FeH_BH:+.2f}.dat"))
-
-        # Grab only stellar type 14
-        BH_mi, BH_mf = bhifmr[:, :2][bhifmr[:, 2] == 14].T
-
-        # linear spline to avoid boundary effects near m_A, m_B, etc
-        self._BH_spline = UnivariateSpline(BH_mi, BH_mf, s=0, k=1, ext=0)
-
-        self.BH_mi = bounds(BH_mi[0], BH_mi[-1])
+        self._BH_spline, self.BH_mi, self.BH_mf = _F12_BH_predictor(FeH)
 
         # self.mBH_min, self.mBH_max = np.min(BH_mf), np.max(BH_mf)
-        self.BH_mf = bounds(np.min(BH_mf), np.inf)
         self.mBH_min = self.BH_mf.lower
 
         # ------------------------------------------------------------------
         # White Dwarfs
         # ------------------------------------------------------------------
 
-        wdgrid = np.loadtxt(get_data("sevtables/wdifmr.dat"))
+        self._WD_spline, self.WD_mi, self.WD_mf = _MIST2018_WD_predictor(FeH)
 
-        # Get the closest model
-        j = np.argmin(np.abs(self.FeH_WD - wdgrid[:, 0]))
-        WD_m_max, WD_coeffs = wdgrid[j, 1], wdgrid[j, 2:]
-
-        self._WD_spline = np.polynomial.Polynomial(WD_coeffs[::-1])
-
-        # TODO polynomial starts misbehaving far above 0, but don't know where
-        self.WD_mi = bounds(0.0, WD_m_max)
-
-        self.WD_mf = bounds(0.0, self._get_WD_bounds(WD_m_max))
         self.mWD_max = self.WD_mf.upper
 
         # ------------------------------------------------------------------
@@ -91,61 +180,6 @@ class IFMR:
 
         self.NS_mi = bounds(self.WD_mi[1], self.BH_mi[0])
         self.NS_mf = bounds(1.4, 1.4)
-
-    def _check_feh_bounds(self):
-        '''Ensure FeH is within model grid and adjust otherwise'''
-
-        mssg = ("{0:.2f} is out of bounds for the {1} metallicity grid, "
-                "falling back to {2} of {3:.2f}")
-
-        # ------------------------------------------------------------------
-        # White Dwarfs
-        # ------------------------------------------------------------------
-
-        wdgrid = np.loadtxt(get_data("sevtables/wdifmr.dat"))[:, 0]
-
-        if self.FeH < (fback := np.min(wdgrid)):
-            logging.debug(mssg.format(self.FeH, 'WD', 'minimum', fback))
-            self.FeH_WD = fback
-
-        elif self.FeH > (fback := np.max(wdgrid)):
-            logging.debug(mssg.format(self.FeH, 'WD', 'maximum', fback))
-            self.FeH_WD = fback
-
-        else:
-            self.FeH_WD = self.FeH
-
-        # ------------------------------------------------------------------
-        # Black Holes
-        # ------------------------------------------------------------------
-
-        bhgrid = np.array([float(fn.stem.split('FEH')[-1])
-                           for fn in get_data('sse').glob('MP*dat')])
-
-        if self.FeH < (fback := np.min(bhgrid)):
-            logging.debug(mssg.format(self.FeH, 'BH', 'minimum', fback))
-            self.FeH_BH = fback
-
-        elif self.FeH > (fback := np.max(bhgrid)):
-            logging.debug(mssg.format(self.FeH, 'BH', 'maximum', fback))
-            self.FeH_BH = fback
-
-        else:
-            self.FeH_BH = self.FeH
-
-    def _get_WD_bounds(self, max_mi):
-        '''compute the WD_mf bounds as best we can from the polynomial'''
-
-        # Compute the max/mins taking derivative of polynomial
-        WD_minmax = self._WD_spline.deriv().roots().real
-
-        # Restrict x (initial mass) to between 1 and max_mi
-        # TODO lower 1 is required to avoid polynomial effects, but is arbitrary
-        #   really it won't matter, maximum will always be far above 1
-        restr = (1. < WD_minmax) & (WD_minmax <= max_mi)
-
-        # Determine the maximum WD final mass (including upper bound in case)
-        return self._WD_spline(np.r_[WD_minmax[restr], max_mi]).max()
 
     def predict_type(self, m_in):
         '''Predict the remnant type (WD, NS, BH) given the initial mass(es)'''
