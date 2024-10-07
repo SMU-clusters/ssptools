2ba415b555f114c71dbf2b38cb2e9b45cc60b149
Nolan Dickson <nolan.dickson@smu.ca>
2024-10-07 20:12:27 -0300

    add tests for all new IFMR prescriptions

--- ssptools/ifmr.py
+++ ssptools/ifmr.py
@@ -251,7 +251,7 @@ def _brokenpl_BH_predictor(exponents=[1, 3, 1], slopes=[1, 6e-4, 0.43],
         BH_line(opt.fminbound(BH_line, *m_breaks[i:i + 2]))
         for i in range(len(exponents))
     ])
-    mfu = np.min([
+    mfu = np.max([
         BH_line(opt.fminbound(lambda mi: -BH_line(mi), *m_breaks[i:i + 2]))
         for i in range(len(exponents))
     ])
